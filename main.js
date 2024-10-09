e4c912173d1a57151c9f032e56e5b1a29c093987
YAMMN <yassin.ahmed.ya0707@gmail.com>
2024-10-09 12:07:27 +0300

    2nd commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById("button");
+let preReg = document.getElementById("pre-reg");
+btn.addEventListener("click",  () => {
+    preReg.classList.add("transform");
+});
+// end animation
+let myForm = document.getElementById("myForm");
+let name = document.querySelector(".name");
+let email = document.querySelector(".email");
+let pass = document.querySelector(".password");
+let button = document.querySelector(".btn");
+
+
+
